48758bb19da2ca998139e32b6c9c32b98dd7a381
eko.zhan <eko.z@hotmail.com>
2018-02-02 09:36:37 +0800

    init

---
+++ src/main/java/com/eastrobot/ocrapi/OcrApiApplication.java
@@ -0,0 +1,12 @@
+package com.eastrobot.ocrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrApiApplication.class, args);
+	}
+}
