9c7b5c30c70effcca0a46fb95a0eb0bd8ca98311
eko.zhan <eko.z@hotmail.com>
2018-04-24 09:57:36 +0800

    Controller code optimized

--- src/main/java/com/eastrobot/ocrapi/web/OcrController.java
+++ src/main/java/com/eastrobot/ocrapi/web/OcrController.java
@@ -3,6 +3,8 @@
  */
 package com.eastrobot.ocrapi.web;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 import java.io.File;
@@ -17,12 +19,14 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import springfox.documentation.service.ResponseMessage;
 
 import com.alibaba.fastjson.JSONObject;
 import com.eastrobot.log.annotation.Interval;
@@ -34,7 +38,7 @@ import com.eastrobot.ocrapi.service.OcrService;
  * @date 2017年12月19日 下午2:33:05
  * @version 1.0
  */
-@Controller
+@RestController
 public class OcrController extends BaseController {
 
 	private static final Log logger = LogFactory.getLog(OcrController.class);
@@ -49,12 +53,17 @@ public class OcrController extends BaseController {
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
-	@ApiOperation(value="传入视频或图片文件流，解析并返回内容", notes="返回json格式{\"summary\":\"You have to stop taking people at their wordl \",\"img\":\"QQ图片20140430203614.jpg\",\"isVideo\":false,\"text\":\"   别再信别人的了 !  You have to stop taking people at their wordl     \",\"keyword\":\"stop people You taking wordl \"}")
-	@RequestMapping(value="/", method=RequestMethod.POST, produces="application/json;charset=utf-8")
-	@ResponseBody
+	@ApiOperation(value="传入视频或图片文件流，解析并返回内容", response=ResponseMessage.class)
+	@ApiImplicitParams({
+	        @ApiImplicitParam(name="file", value="待转换文件", dataType="__file", required=true, paramType="form")
+	})
+	@PostMapping(
+			value = "/",
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE},
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}
+    )
 	@Interval
-	public JSONObject parse(MultipartHttpServletRequest request) throws IOException, InterruptedException{
-		MultipartFile file = request.getFile("file");
+	public JSONObject parse(@RequestParam("file") MultipartFile file) throws IOException, InterruptedException{
 		String folder = ocrService.getFolderString();
 		
 		String inputPath = folder + "/" + file.getOriginalFilename();
@@ -65,9 +74,8 @@ public class OcrController extends BaseController {
 	}
 	
 	@ApiOperation(value="根据指定的图片路径返回图片文件流")
-	@RequestMapping(value="/ocr/loadImg", method=RequestMethod.GET)
-	@ResponseBody
-	public void loadImg(String path, HttpServletResponse response) throws IOException{
+	@GetMapping("/ocr/loadImg")
+	public void loadImg(@RequestParam String path, HttpServletResponse response) throws IOException{
 		String folder = ocrService.getFolderString();
 		File file = new File(folder + "/" + path);
 		FileInputStream inputStream = new FileInputStream(file);
@@ -78,9 +86,8 @@ public class OcrController extends BaseController {
 	}
 	
 	@ApiOperation(value="根据指定的视频路径返回视频文件流")
-	@RequestMapping(value="/ocr/loadVideo", method=RequestMethod.GET)
-	@ResponseBody
-	public void loadVideo(String path, HttpServletResponse response) throws IOException{
+	@GetMapping("/ocr/loadVideo")
+	public void loadVideo(@RequestParam String path, HttpServletResponse response) throws IOException{
 		String folder = ocrService.getFolderString();
 		File file = new File(folder + "/" + path);
 		FileInputStream inputStream = new FileInputStream(file);
